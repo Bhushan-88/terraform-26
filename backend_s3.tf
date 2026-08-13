b7267a8d404fbad93638073c86d9c4c32041f84a
Bhushan-88 <bhushabdurgawli1@gmail.com>
2026-08-13 22:38:09 +0530

    add comments to backend_s3.tf, provider.tf, and variables.tf for better clarity

--- backend_s3.tf
+++ backend_s3.tf
@@ -1,4 +1,4 @@
-
+# This file contains the Terraform configuration for creating an S3 bucket to be used as a backend for storing Terraform state files.
 resource "aws_s3_bucket" "terraform_backend" {
   bucket        = var.backend_bucket_name
   force_destroy = true
@@ -8,7 +8,7 @@ resource "aws_s3_bucket" "terraform_backend" {
     Purpose = "Terraform state"
   }
 }
-
+# This resource block creates an S3 bucket with the name specified in the `backend_bucket_name` variable. The `force_destroy` attribute is set to true, which allows the bucket to be deleted even if it contains objects. The bucket is also tagged with a name and purpose for easier identification.
 resource "aws_s3_bucket_versioning" "terraform_backend" {
   bucket = aws_s3_bucket.terraform_backend.id
 
@@ -16,7 +16,7 @@ resource "aws_s3_bucket_versioning" "terraform_backend" {
     status = "Enabled"
   }
 }
-
+# This resource block enables versioning for the S3 bucket created above. Versioning allows you to keep multiple versions of an object in the same bucket, which can be useful for recovering from accidental deletions or overwrites.
 resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_backend" {
   bucket = aws_s3_bucket.terraform_backend.id
 
@@ -26,7 +26,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_backend
     }
   }
 }
-
+# This resource block enables server-side encryption for the S3 bucket using the AES256 algorithm. This ensures that all objects stored in the bucket are encrypted at rest, providing an additional layer of security for your Terraform state files.
 resource "aws_s3_bucket_public_access_block" "terraform_backend" {
   bucket = aws_s3_bucket.terraform_backend.id
 
--- provider.tf
+++ provider.tf
@@ -1,3 +1,4 @@
+# This file contains the provider configuration for Terraform, specifying the required Terraform version and the AWS provider version. It also sets the AWS region to be used for creating resources.
 terraform {
   required_version = ">= 1.15.8"
 
@@ -8,7 +9,8 @@ terraform {
     }
   }
 }
-
+# This block specifies the required Terraform version and the AWS provider version. 
+# It ensures that the configuration is compatible with Terraform version 1.15.8 or higher and uses the AWS provider version 6.x.
 provider "aws" {
   region = "us-east-1"
 }
