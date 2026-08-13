ef73b93ca4f5ff7ce173a0ce61d125b47889bc3d
Bhushan-88 <bhushabdurgawli1@gmail.com>
2026-08-13 22:13:35 +0530

    refactor variables.tf to update S3 backend bucket configuration and remove unused variables

--- variables.tf
+++ variables.tf
@@ -1,29 +1,11 @@
 variable "aws_region" {
-  description = "AWS region where the instance will be launched"
+  description = "AWS region for the S3 backend bucket"
   type        = string
   default     = "us-east-1"
 }
 
-variable "ami_id" {
-  description = "AMI ID for the EC2 instance (Ubuntu 22.04 LTS in us-east-1)"
+variable "backend_bucket_name" {
+  description = "Globally unique name for the Terraform backend S3 bucket"
   type        = string
-  default     = "ami-0f3caa1cf4417e51b"
-}
-
-variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  default     = "t3.micro"
-}
-
-variable "instance_name" {
-  description = "Name tag for the EC2 instance"
-  type        = string
-  default     = "my-web-server"
-}
-
-variable "key_pair_name" {
-  description = "Name of the existing AWS Key Pair to use for SSH access"
-  type        = string 
-    default     = "Virginia-key"  # Replace with your actual key pair name
+  default     = "my-terraform-backend-bucket-123456"
 }
