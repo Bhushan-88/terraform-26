6ca1b1e6b26819a578fafe6f4532a135774cd426
Bhushan-88 <bhushabdurgawli1@gmail.com>
2026-08-17 19:19:28 +0530

    feat: update S3 module to use variable for bucket name and add DynamoDB module structure

--- terraform-aws-multi-env/modules/s3/main.tf
+++ terraform-aws-multi-env/modules/s3/main.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "my_bucket" {
   bucket = "test-my-app-bucket-100"
 
   tags = {
-  name = "test-my-app-bucket-100"
+  name = var.aws_s3_bucket_name
   }
 }
 
--- terraform-aws-multi-env/modules/s3/variables.tf
+++ terraform-aws-multi-env/modules/s3/variables.tf
@@ -1,3 +1,6 @@
-variable "aws_s3_bucket" {
-  
+variable "aws_s3_bucket_name" {
+  description = "this variable holds s3 bucket name"
+  default = "test-my-app-bucket-100"
+  type = string
 }
+
