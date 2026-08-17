8d2fcebd123b08e21d73cb56f694b7f2d39cfe0b
Bhushan-88 <bhushabdurgawli1@gmail.com>
2026-08-17 18:55:29 +0530

    feat: add EC2 module with configuration ec2 and s3

---
+++ terraform-aws-multi-env/modules/ec2/variables.tf
@@ -0,0 +1,15 @@
+variable "ec2_instance_name" {
+  description = "Name of the EC2 instance"
+  type        = string
+  default     = "terra-auto-server"
+}
+variable "ec2_instance_volume_size" {
+  description = "Size of the EC2 instance root volume in GB"
+  type        = number
+  default     = 8
+}
+variable "ec2_instance_state" {
+  description = "Desired state of the EC2 instance"
+  type        = string
+  default     = "running"
+}
