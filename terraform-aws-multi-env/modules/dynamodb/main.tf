08dd14f20524182d7c311c50d2b04830a08c64e6
Bhushan-88 <bhushabdurgawli1@gmail.com>
2026-08-21 14:36:15 +0530

    feat: update DynamoDB table hash key and attribute name to use LockID

--- terraform-aws-multi-env/modules/dynamodb/main.tf
+++ terraform-aws-multi-env/modules/dynamodb/main.tf
@@ -1,9 +1,9 @@
 resource "aws_dynamodb_table" "target-corporate-table" {
   name           = var.dynamodb_table_name
   billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "id"
+  hash_key       = "LockID"
   attribute {
-    name = "id"
+    name = "LockID"
     type = "S"
   }
   
