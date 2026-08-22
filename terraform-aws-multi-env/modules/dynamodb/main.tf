2359e67a4214cce0e31ded398148022a1a6a988e
Bhushan-88 <bhushabdurgawli1@gmail.com>
2026-08-22 16:15:40 +0530

    feat: prepend environment prefix to DynamoDB table name

--- terraform-aws-multi-env/modules/dynamodb/main.tf
+++ terraform-aws-multi-env/modules/dynamodb/main.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "target-corporate-table" {
-  name           = "${var.dynamodb_table_name}-${count.index + 1}"
+  name           = "${var.env}-${var.dynamodb_table_name}-${count.index + 1}"
   count          = var.dynamodb_table_count
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "LockID"
