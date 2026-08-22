f3b5b366fde0d769a6fc2b1b2d6d70b96c28b7d3
Bhushan-88 <bhushabdurgawli1@gmail.com>
2026-08-22 16:13:10 +0530

    feat: update S3 bucket naming to include environment prefix

--- terraform-aws-multi-env/modules/s3/main.tf
+++ terraform-aws-multi-env/modules/s3/main.tf
@@ -1,9 +1,9 @@
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "${var.aws_s3_bucket_name}-${count.index + 1}"
+  bucket = "${var.env}-${var.aws_s3_bucket_name}-${count.index + 1}"
   count = var.s3_bucket_count #2
 
   tags = {
-  name = "${var.aws_s3_bucket_name}-${count.index + 1}"
+  name = "${var.env}-${var.aws_s3_bucket_name}-${count.index + 1}"
   environment = var.env
   }
 }
