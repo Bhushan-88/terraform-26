2f8fc151fa7509cefc0b9a2158986e9524b73caf
Bhushan-88 <bhushabdurgawli1@gmail.com>
2026-08-22 14:38:48 +0530

    Add Terraform state file for staging environment with EC2 instance configuration
    - Created terraform.tfstate for staging environment
    - Configured AWS resources including EC2 instances, VPC, security groups, and key pairs
    - Utilized Ubuntu 24.04 AMI for EC2 instances
    - Set up security group rules for HTTP and SSH access

--- terraform-aws-multi-env/main.tf
+++ terraform-aws-multi-env/main.tf
@@ -1,33 +1,39 @@
 locals {
+    env = {
 
-    dev = {
-        ec2_instance_name = "dev-terra-auto-server"
-        ec2_instance_key_name = "dev-terra-auto-key"
-        ec2_instance_volume_size = 8
-        ec2_instance_state = "running"
-        ec2_instance_type = "t3.micro"
-        ec2_instance_count = 2
+        dev = {
+            ec2_instance_name = "dev-terra-auto-server"
+            ec2_instance_key_name = "dev-terra-auto-key"
+            ec2_instance_volume_size = 8
+            ec2_instance_state = "running"
+            ec2_instance_type = "t3.micro"
+            ec2_instance_count = 1
+        }
+        stg = {
+            ec2_instance_name = "stg-terra-auto-server"
+            ec2_instance_key_name = "stg-terra-auto-key"
+            ec2_instance_volume_size = 8
+            ec2_instance_state = "running"
+            ec2_instance_type = "t3.micro"
+            ec2_instance_count = 2
+        }
+        prd = {
+            ec2_instance_name = "prd-terra-auto-server"
+            ec2_instance_key_name = "prd-terra-auto-key"
+            ec2_instance_volume_size = 8
+            ec2_instance_state = "running"
+            ec2_instance_type = "t3.micro"
+            ec2_instance_count = 3
+        }
     }
-    stg = {
-        ec2_instance_name = "stg-terra-auto-server"
-        ec2_instance_key_name = "stg-terra-auto-key"
-        ec2_instance_volume_size = 8
-        ec2_instance_state = "running"
-        ec2_instance_type = "t3.micro"
-        ec2_instance_count = 1
-    }
-    prd = {
-        ec2_instance_name = "prd-terra-auto-server"
-        ec2_instance_key_name = "prd-terra-auto-key"
-        ec2_instance_volume_size = 8
-        ec2_instance_state = "running"
-        ec2_instance_type = "t3.micro"
-        ec2_instance_count = 1
-    }
+    current = lookup(local.env, terraform.workspace, local.env["dev"])
 }
 
 module "ec2" {
   source = "./modules/ec2"
-  env = terraform.workspace #dev this value will be used to select the environment
-  ec2_instance_count = local.dev.ec2_instance_count
+  env = terraform.workspace #This value will be dev, stg, prd based on the workspace selected
+  ec2_instance_count = local.current.ec2_instance_count
+}
+module "s3" {
+  
 }
