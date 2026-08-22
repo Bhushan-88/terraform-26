61de8aa087137a196f51cd22a51d1e86bd98a2f6
Bhushan-88 <bhushabdurgawli1@gmail.com>
2026-08-22 15:03:08 +0530

    feat: add dynamic count for DynamoDB table instances

--- terraform-aws-multi-env/modules/dynamodb/main.tf
+++ terraform-aws-multi-env/modules/dynamodb/main.tf
@@ -1,5 +1,6 @@
 resource "aws_dynamodb_table" "target-corporate-table" {
   name           = var.dynamodb_table_name
+  count          = "${var.dynamodb_table_count}-${count.index + 1}"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "LockID"
   attribute {
